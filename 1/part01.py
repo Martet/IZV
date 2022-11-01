f5e8ca3620fc9a44ee55d5037eb7372e501db13a
Martin Zmitko <mzmitko@redhat.com>
2022-11-01 18:58:37 +0100

    Add tasks 1, 4, 5

--- 1/part01.py
+++ 1/part01.py
@@ -18,20 +18,41 @@ from typing import List
 
 
 def integrate(x: np.array, y: np.array) -> float:
+    return np.sum((x[1:] - x[:-1]) * ((y[:-1] + y[1:]) / 2))
+
+
+def generate_graph(
+    a: List[float], show_figure: bool = False, save_path: str | None = None
+):
     pass
 
 
-def generate_graph(a: List[float], show_figure: bool = False, save_path: str | None=None):
-    pass
-
-
-def generate_sinus(show_figure: bool=False, save_path: str | None=None):
+def generate_sinus(show_figure: bool = False, save_path: str | None = None):
     pass
 
 
 def download_data(url="https://ehw.fit.vutbr.cz/izv/temp.html"):
-    pass
+    r = requests.get(url)
+    r.raise_for_status()
+    bs = BeautifulSoup(r.text, "html.parser")
+    data = []
+    for row in bs.find_all(attrs={"class": "ro1"}):
+        row_dict = {}
+        cols = [col for col in row.find_all("td") if col.find("p")]
+        row_dict["year"] = int(cols[0].p.string)
+        row_dict["month"] = int(cols[1].p.string)
+        row_dict["temp"] = np.array(
+            [float(col.p.string.replace(",", ".")) for col in cols[2:]]
+        )
+        data.append(row_dict)
+    return data
 
 
 def get_avg_temp(data, year=None, month=None) -> float:
-    pass
+    if year is not None:
+        data = [d for d in data if d["year"] == year]
+    if month is not None:
+        data = [d for d in data if d["month"] == month]
+
+    data = np.concatenate([d["temp"] for d in data], axis=None)
+    return np.average(data)
